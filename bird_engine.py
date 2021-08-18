9180aff9f46c524f3e972e3f2b9f506585add655
ChangNoi <44426331+RioRocker97@users.noreply.github.com>
2021-08-18 22:00:38 +0700

    going well

--- bird_engine.py
+++ bird_engine.py
@@ -1,48 +1,44 @@
-import argparse,tqdm,time,threading,subprocess,shutil,os
+from os import getcwd,environ,path,makedirs
 from yolo_detect import prepareYolo,runYolo
-#subprocess.run('cls',shell=True)
-def progress_bar():
-    for i in tqdm.tqdm(range(100),ncols=70,desc="anything"):
-        num=0
-        for i in range(0,1000000):
-            num+=1
-def progress_bar2():
-    for i in tqdm.tqdm(range(100),ncols=70,desc="something"):
-        num=0
-        for i in range(0,10000):
-            num+=1
-def my_progress(name,rag):
-    for i in tqdm.tqdm(range(100),ncols=70,desc=name):
-        num=0
-        for i in range(0,rag):
-            num+=1
-def cmd_usage():
-    arg = argparse.ArgumentParser(usage="Something")
-    opt = arg.parse_args()
-    return opt
-# just trying out tqdm with muti-thred . IDK im suck at mutithreading
-def test_fx():
-    task1 = threading.Thread(target=my_progress,args=("Task 1",1000000))
-    task2 = threading.Thread(target=my_progress,args=("Task 2",10000))
-    task3 = threading.Thread(target=my_progress,args=("Task 3",100000))
-    task4 = threading.Thread(target=my_progress,args=("Task 4",100000))
-    task1.start()
-    task2.start()
-    task3.start()
-    task4.start()
-# real deal
+from google.cloud import storage
+
+BIRB_MODEL = getcwd()+'/birb_model/'
+TEMP = getcwd()+'/temp/'
+GCP_BUCKET = environ['GCP_B']
+GCP_BUCKET_URL = environ['GCP_B_URL']
+#LINE_ACCESS_TOKEN = os.environ['LINE_TOKEN']
+#BIRB_BUCKET = 0
 def preload():
-    if not os.path.exists(os.getcwd()+'/birb_model/'):
-        os.makedirs(os.getcwd()+'/birb_model/')
-        my_progress("Get all model",100000)
-    if not os.path.exists(os.getcwd()+'/temp/'):
-        os.makedirs(os.getcwd()+'/temp/')
+    if not path.exists(BIRB_MODEL):
+        makedirs(BIRB_MODEL)
+        # do download or update birb model
+        #my_progress("Get all model",100000)
+    if not path.exists(BIRB_MODEL):
+        makedirs(BIRB_MODEL)
+        #store image here before detection
+    
+    #cut to pre-load single model for now...
+    prepareYolo(BIRB_MODEL+'bird_first_gather.pt',confidence=0.7)
+def upload_img(img_id,data):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(GCP_BUCKET)
+    blob = bucket.blob(str(img_id)+'.jpg')
+
+    try:
+        blob.upload_from_string(data,content_type='image/jpeg')
+        print('Imaged Upload !')
+        return GCP_BUCKET_URL+str(img_id)+'.jpg'
+    except:
+        print('ERROR uploading')
+        return "ERROR"
+def detect(img_file):
+    """
+        This function should return finished detection image data
+    """
+    return runYolo(TEMP+img_file)
 
 
 if __name__ == "__main__":
-    subprocess.run('cls',shell=True)
-    preload()
+    pass
 
-# everything goes here
-# i want this to be beautiful while running in CMD
 
--- webhook.py
+++ webhook.py
@@ -1,1 +1,69 @@
-# for creating bird_classifier's API
+import urllib3,json
+from flask import Flask,Response,request
+from bird_engine import *
+app = Flask(__name__)
+HTTP = urllib3.PoolManager()
+COMMON_HEADER = {
+    'Authorization': 'Bearer '+ environ['LINE_TOKEN']
+}
+def push_text_msg(user,flex_body):
+    this_header = COMMON_HEADER
+    this_header['Content-Type'] = 'application/json'
+    data = json.dumps({
+        "to":user,
+        "messages":[{
+            "type":"flex",
+            "altText": "Reverse Image search in Flex style",
+            "contents": flex_body     
+        }]
+    })
+    rep = HTTP.request('POST','https://api.line.me/v2/bot/message/push',headers=this_header,body=data)
+
+    if str(rep.status) == '200':
+        print('push reply {',user,'} OK')
+    else:
+        print(rep.data)
+        raise Exception('push reply ERROR')
+def text_reply(reply_token,msg="Another One Webhook"):
+    this_header = COMMON_HEADER
+    this_header['Content-Type'] = 'application/json'
+    data = json.dumps({
+        "replyToken": reply_token,
+        "messages":[
+            {
+                "type":"text",
+                "text":msg
+            }
+        ]
+    })
+    rep = HTTP.request('POST','https://api.line.me/v2/bot/message/reply',
+        body = data,headers=this_header)
+    
+    if str(rep.status) == '200':
+        print('Reply OK')
+    else :
+        print(rep.data)
+        print('Reply ERROR')
+
+@app.route('/', methods=['POST'])
+def respond():
+    line_events = request.json['events']
+    for event in line_events:
+        if event['type']=='message':
+            token = event['replyToken']
+            address = event['source']
+            payload = event['message']
+            if payload['type'] == 'text':
+                print("from : ",address['userId'])
+                print("Text msg : ",payload['text'])
+                text_reply(token)
+            if payload['type'] == 'image':
+                user_id = address['userId']
+                img_id = payload['id']
+                print("LINE image ID : ",img_id)
+                text_reply(token,msg="Searching.... Please Wait")
+
+    return Response(status=200)
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0',port=80)
